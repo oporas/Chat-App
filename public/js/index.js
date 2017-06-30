a836de26e9e646075ffc43998dce7cb5ec18d844
Olli-Pekka Oras <Olli-Pekka.Oras@conmio.com>
2017-06-30 10:01:02 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('connected to server');
-
-    socket.emit('createMessage', {
-        from: 'test@test.com',
-        text: 'Testing jooh'
-    })
 });
 
 socket.on('disconnect', function () {
