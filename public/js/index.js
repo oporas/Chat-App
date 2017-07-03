301c3ca995337dfdcda941f3df85c0626b747424
Olli-Pekka Oras <Olli-Pekka.Oras@conmio.com>
2017-07-03 16:16:57 +0300

    Added message form

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,25 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log('New message', message);
+    var li = $('<li></li>')
+    li.text(`${message.from}: ${message.text}`);
+
+    $('#chat').append(li)
 });
+
+// socket.emit('createMessage', {
+//     from: 'Frank',
+//     text: 'Hi'
+// }, function (data) {
+//     console.log('Got it', data);
+// })
+
+$('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, function (data) {
+        console.log('Got it', data);
+    })
+})
