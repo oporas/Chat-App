5716bfb9c15a57a75f68952c89686e2f49f97fc0
Olli-Pekka Oras <Olli-Pekka.Oras@conmio.com>
2017-10-29 20:08:13 +0200

    Display images in chat

--- app/scripts/message.jsx
+++ app/scripts/message.jsx
@@ -9,10 +9,15 @@ module.exports = class Message extends React.Component {
     getMessageContent(message) {
         const formattedTime = moment(message.createdAt).format('h:mm a');
         const className = message.from == 'System' ? 'system-message' : '';
+        const imageRegex = /(https?:\/\/.*\.(?:png|jpg|gif|jpeg|webp))/i;
+        const hasImage = imageRegex.exec(message.text);
         if (message.type === 'location') {
             return <li className={className}>{formattedTime}: {message.from}: <a target="_black" href="{message.url}">My location</a></li>;
         }
-        return  <li className={className}>{formattedTime}: {message.from}: {message.text}</li>;
+        if (hasImage) {
+            return <li className={className}>{formattedTime}: {message.from}: {message.text} <img className="image" src={hasImage[0]} /></li>;
+        }
+        return <li className={className}>{formattedTime}: {message.from}: {message.text}</li>;
     }
 
     render() {
